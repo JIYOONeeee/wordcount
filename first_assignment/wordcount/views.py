b8a72c174d2f62b09b90cddb2b4259e859beb77a
JIYOONeeee <jennygood111@naver.com>
2019-01-21 18:30:11 +0900

    first commit

---
+++ first_assignment/wordcount/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def result(request):
+    text = request.GET['fulltext']
+   
+   
+    words = text.split()
+    word_dictionary={}
+
+    for word in words:
+        
+        if word in word_dictionary:
+           #increase 
+           word_dictionary[word]+=1
+        else:
+            #add in dictionary
+            word_dictionary[word]=1
+
+    return render(request,'result.html',{'full':text,'total':len(words),'totalword':len(text),'dictionary':word_dictionary.items()})
+
+def search(request):
+
+ searching = request.GET['search']
+ return render(request,'search.html',{'ss':searching})    
